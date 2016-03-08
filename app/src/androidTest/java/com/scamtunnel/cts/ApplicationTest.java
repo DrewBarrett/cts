98c2c88cd7568509e5a9872abb5ce5dec1e95824
Drew Barrett <dfb1998@gmail.com>
2016-03-08 07:14:23 -0500

    before merge

--- app/src/androidTest/java/com/scamtunnel/cts/ApplicationTest.java
+++ app/src/androidTest/java/com/scamtunnel/cts/ApplicationTest.java
@@ -2,7 +2,11 @@ package com.scamtunnel.cts;
 
 import android.app.Application;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.test.ApplicationTestCase;
+
+import java.util.Set;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -11,8 +15,11 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
         }
+// If there are paired devices
+
     }
 }
--- app/src/main/java/com/scamtunnel/cts/Main2Activity.java
+++ app/src/main/java/com/scamtunnel/cts/Main2Activity.java
@@ -31,6 +31,7 @@ public class Main2Activity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
